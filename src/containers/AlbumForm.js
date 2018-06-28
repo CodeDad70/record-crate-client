ef24c99a565c6ca7ba8bb896eb2854786c438e08
CodeDad <github email address>
2018-06-27 22:10:24 -0400

    created a store

---
+++ src/containers/AlbumForm.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class AlbumForm extends Component {
+  render(){
+    return(
+      <div>
+        Add an Album
+        <form>
+
+        </form>
+      </div>
+    )
+  }
+}
+
+export default AlbumForm
--- src/containers/Albums.js
+++ src/containers/Albums.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './Albums.css';
 import AlbumCard from '../components/AlbumCard'
+import AlbumForm from './AlbumForm'
 
-const Albums = (props) => (
-  <div>
-    <h1>Albums I Own </h1>
-      {props.albums.map(album => <AlbumCard key={album.id} album={album} />)}  
-  </div>
-);
-
+class Albums extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Albums I Own </h1>
+          {this.props.albums.map(album => <AlbumCard key={album.id} album={album} />)}
+          <AlbumForm/>  
+      </div>
+    )
+  }
+}
 export default Albums;
 
