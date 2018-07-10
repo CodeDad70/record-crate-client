f7bc3fd5f4635eedecc0c7421a895ec14edaa605
CodeDad <github email address>
2018-07-10 16:09:01 -0400

    installed react-redux and added Provider to index.js

--- src/store.js
+++ src/store.js
@@ -2,6 +2,28 @@ import {
   createStore,
   applyMiddleware,
   combineReducers
-} from 'redux'
+} from 'redux';
+import thunk from 'redux-thunk';
+
+const albumsReducer = (state =[], action) => {
+  switch(action.type) {
+    case 'GET_ALBUMS_SUCCESS':
+      return action.albums;
+
+  default:
+    return state    
+  }
+}
+
+const reducers = combineReducers ({
+  album: albumsReducer
+});
+const middleware = [thunk]; 
+
+export default createStore (
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(...middleware),
+);
 
 
